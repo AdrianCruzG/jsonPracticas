e57c030c136eaf3829c3dfe49d7842d73813ddac
Alumno <Alumno@DESKTOP-RRHDRFS>
2024-03-27 14:11:18 +0100

    Agregar archivos de proyecto.

---
+++ jsonPracticas1/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "https://jsonplaceholder.typicode.com/photos/",
+        dataType: "json",
+        success: function (data) {
+
+            $.each(data, function (i, item) {
+                var row = "<tr>" +
+                    "<td>" + item.albumid + "</td>" +
+                    "<td>" + item.id + "</td>" +
+                    "<td>" + item.title + "</td>" +
+                    "<td> <img src='" + item.url + "'/></td>" +
+                    "<td> <img src='" + item.thumbnailurl + "'/></td>" +
+                    "</tr>";
+                $("#tabla>tbody").append(row);
+            });
+        }, //End of AJAX Success function  
+    });
+}); 
